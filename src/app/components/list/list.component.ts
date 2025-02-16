204f7800c3fb7ce17c1a1f18d87a493e074bd5f5
unknown <kianazolfaghari79@gamil.com>
2025-02-16 15:09:23 +0330

    fix

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,22 +1,23 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Component, inject, model, OnInit, signal } from '@angular/core';
-import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
-import { AuthService } from '../../authservice/auth.service';
-import { LibraryService } from '../../services/library.service';
-import { HttpService } from '../../services/httpServices/get.service';
-import { PostService } from '../../services/httpServices/post.service';
-import { bookData } from '../../services/dataTypes/bookdata.service';
-import { Title } from '@angular/platform-browser';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Component, inject, model, OnInit, signal } from "@angular/core";
+import { Router } from "@angular/router";
+import Swal from "sweetalert2";
+import { AuthService } from "../../authservice/auth.service";
+import { LibraryService } from "../../services/library.service";
+import { HttpService } from "../../services/httpServices/get.service";
+import { PostService } from "../../services/httpServices/post.service";
+import { bookData } from "../../services/dataTypes/bookdata.service";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.scss',
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrl: "./list.component.scss"
 })
 export class ListComponent implements OnInit {
   books: any[] = [];
   isLoading: boolean = false;
+
   bookData = {
     totalBooks: 0,
     page: 0,
@@ -24,24 +25,33 @@ export class ListComponent implements OnInit {
     totalPages: 0,
     data: [
       {
-        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-        title: '',
-        summary: '',
-        author: '',
+        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        title: "",
+        summary: "",
+        author: "",
         price: 0,
-        quantity: 0,
-      },
-    ],
+        quantity: 0
+      }
+    ]
   };
-
 
   constructor(
     private http: HttpClient,
     private authService: AuthService,
     private library: LibraryService,
     private httpService: HttpService,
-    private postService:PostService
+    private postService: PostService
   ) {}
+  data: bookData[] = [
+    {
+      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      title: "",
+      summary: "",
+      author: "",
+      price: 0,
+      quantity: 0
+    }
+  ];
 
   ngOnInit(): void {
     this.fetchBooks();
@@ -49,21 +59,24 @@ export class ListComponent implements OnInit {
 
   fetchBooks(): void {
     this.isLoading = true;
-    let params=new HttpParams();
-    params= params.append("page",1).append("limit", 10)
+    const queryParams = {
+      page: 1,
+      limit: 10,
+      Title: "",
+      minPrice: 0,
+      maxPrice: 0
+    };
 
     this.http
-      .get('http://localhost:3000/book', { params })
-      .subscribe({next:
-        (response: any) => {
-          this.books = response.data;
+      .get("http://localhost:3000/book", { params: queryParams })
+      .subscribe({
+        next: (response: any) => {
           this.isLoading = false;
         },
-        error:(error: any) => {
+        error: (error: any) => {
           this.isLoading = false;
         }
-      }
-      );
+      });
   }
   openDialog() {
     this.library.openDialog();
@@ -72,13 +85,13 @@ export class ListComponent implements OnInit {
   exit() {
     this.authService.logout();
   }
-  apiUrl = 'http://localhost:3000/book';
+  apiUrl = "http://localhost:3000/book";
   deleteBook(id: string) {
     this.http.delete(`${this.apiUrl}/${id}`).subscribe({
       next: () => {
         console.log(`Book ${id} deleted`);
-        this.books = this.books.filter((book) => book.id !== id);
-      },
+        this.books = this.books.filter(book => book.id !== id);
+      }
     });
   }
 }
--- src/app/services/dataTypes/bookdata.service.ts
+++ src/app/services/dataTypes/bookdata.service.ts
@@ -1,9 +1,9 @@
 export interface bookData {
       id: '3fa85f64-5717-4562-b3fc-2c963f66afa6';
-      title: '';
-      summary: '';
-      author: '';
-      price: 0;
-      quantity: 0;
+      title:string;
+      summary: string;
+      author:string;
+      price: number;
+      quantity: number;
     }
 
